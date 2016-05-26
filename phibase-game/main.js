e5a4c65909470cda2461e66fef062e6085747def
Siri0n <infernalsirion@yandex.ru>
2016-05-26 14:13:39 +0300

    Graphics overhaul

--- phibase-game/main.js
+++ phibase-game/main.js
@@ -1,12 +1,13 @@
 var Phaser = require("phaser");
 
-var game = new Phaser.Game(800, 600, Phaser.AUTO, '');
+var game = new Phaser.Game(800, 600, Phaser.AUTO, '', "game", true);
 
 game.state.add("game", (function(){
 	return {
 		preload(){
-/*			game.load.spritesheet("testball", "assets/testball.png", 64, 64);
-			game.load.start();*/
+			game.load.spritesheet("tile", "assets/tile.png", 48, 72);
+			game.load.image("dot", "assets/dot.png");
+			game.load.start();
 		},
 		create(){
 			var field = window.field = new Field({
@@ -22,33 +23,44 @@ game.state.add("game", (function(){
 	}
 })());
 
-game.state.start("game");
 
 
 function Field({game, rect}){
 	const grid = {
-		tileWidth: 52,
-		tileHeight: 78,
-		margin: 16
+		tileWidth: 48,
+		tileHeight: 72,
+		margin: 6
 	};
-	var cols = 8;
-	var rows = 5;
-	var dotcol = 5;
+	var cols = 11;
+	var rows = 6;
+	var dotcol = 6;
 
 	var group = game.add.group();
-	function adjust(rect){
-		var width = (grid.tileWidth + grid.margin)*cols - grid.margin;
-		var height = (grid.tileHeight + grid.margin)*rows - grid.margin;
-		var scale = Math.min(rect.w/width, rect.h/height);
-		width *= scale;
-		height *= scale;
-		group.x = rect.x + (rect.w - width)/2;
-		group.y = rect.y + (rect.h - height)/2;
-		group.scale.x = group.scale.y = scale;
+
+	var dots = game.add.group(group);
+	dots.x = (dotcol - 1)*(grid.tileWidth + grid.margin);
+	for(let i = 0; i < rows; i++){
+		let img = game.add.image(-grid.margin/2, i*(grid.tileHeight + grid.margin) + grid.tileHeight, "dot");
+		img.anchor.x = img.anchor.y = 0.5;
+		dots.add(img);
 	}
-	adjust(rect);
 	var tiles = [];
+	var afterTweenStop = [];
+	var tweenCounter = 0;
 	var events = {
+		beginTween(){
+			tweenCounter++;
+			console.log(tweenCounter);
+		},
+		endTween(){
+			console.log(--tweenCounter);
+			if(!tweenCounter){
+				console.log("lol");
+				while(afterTweenStop.length){
+					afterTweenStop.pop()();
+				}
+			}
+		},
 		click(tile){
 			var i = tile.y,
 				j = tile.x;
@@ -66,13 +78,15 @@ function Field({game, rect}){
 				tiles[i][j + 2].switch();
 				tiles[i][j + 1].moveAndBack(-1, 0);
 				tiles[i][j + 2].moveAndBack(-2, 0);
+				afterTweenStop.push(() => adjust(rect));
 			}else{
-				tiles[i][j + 1].moveAndBack(-1, 0, function(){
+				tiles[i][j + 1].moveAndBack(-1, 0, true);
+				tiles[i][j + 2].moveAndBack(-2, 0, true);
+				afterTweenStop.push(function(){
 					tile.switch();
 					tiles[i][j + 1].switch();
-				});
-				tiles[i][j + 2].moveAndBack(-2, 0, function(){
 					tiles[i][j + 2].switch();
+					adjust(rect);
 				});
 			}
 		}
@@ -80,53 +94,123 @@ function Field({game, rect}){
 	for(let i = 0; i < rows; i++){
 		let row = [];
 		for(let j = 0; j < cols; j++){
+			let value;
+			if(j == 0 || j > cols - 3){
+				value = 0;
+			}else{
+				value = Math.random() > 0.5 ? 0 : 1;
+			}
 			row.push(new Tile({
 				game,
 				group,
 				grid,
 				x: j,
 				y: i,
-				value: Math.random() > 0.5 ? 1 : 0,
+				value,
 				events
 			}));
 		}
 		tiles.push(row);
 	}
-	
-	this.test = function(i, j){
-		tiles[i][j].value = 1 - tiles[i][j].value;
+	function adjust(rect, init){
+		if(columnHasUnit(cols-1) || columnHasUnit(cols-2)){
+			addColumn();
+			if(columnHasUnit(cols-2)){
+				addColumn();
+			}
+		}else{
+			while(!columnHasUnit(cols-1) && !columnHasUnit(cols-2) && !columnHasUnit(cols-3)){
+				removeColumn();
+			}
+		}
+
+		if(columnHasUnit(0)){
+			addColumn(true);
+		}else{
+			while(!columnHasUnit(0) && !columnHasUnit(1)){
+				removeColumn(true);
+			}
+		}
+
+		var width = (grid.tileWidth + grid.margin)*cols - grid.margin;
+		var height = (grid.tileHeight + grid.margin)*rows - grid.margin;
+		var scale = Math.min(rect.w/width, rect.h/height);
+		width *= scale;
+		height *= scale;
+		if(init){
+			group.x = rect.x + (rect.w - width)/2;
+			group.y = rect.y + (rect.h - height)/2;
+			group.scale.x = group.scale.y = scale;
+			dots.x = dotcol*(grid.tileWidth + grid.margin);
+		}else{
+			game.add.tween(group).to({x: rect.x + (rect.w - width)/2, y: rect.y + (rect.h - height)/2}, 300, "Linear", true);
+			game.add.tween(group.scale).to({x: scale, y: scale}, 300, "Linear", true);
+			game.add.tween(dots).to({x: dotcol*(grid.tileWidth + grid.margin)}, 300, "Linear", true);
+		}
+	}
+	adjust(rect, true);
+
+	function columnHasUnit(i){
+		var f = false;
+		for(let j = 0; j < rows; j++){
+			f = f || tiles[j][i].value;
+		}
+		return f;
+	}
+
+	function addColumn(leftSide){
+		cols++;
+		leftSide && dotcol++;
+		for(let i = 0; i < rows; i++){
+			let newTile = new Tile({
+				game,
+				group,
+				grid,
+				x: cols-1,
+				y: i,
+				value: 0,
+				events
+			})
+			if(leftSide){
+				newTile.x = -1;
+				tiles[i].unshift(newTile);
+				tiles[i].forEach((tile, index) => tile.moveX(index));
+			}else{
+				tiles[i].push(newTile);
+			}
+		}
+	}
+	function removeColumn(leftSide){
+		cols--;
+		leftSide && dotcol--;
+		for(let i = 0; i < rows; i++){
+			if(leftSide){
+				tiles[i].shift().destroy();
+				tiles[i].forEach((tile, index) => tile.moveX(index));
+			}else{
+				tiles[i].pop().destroy();
+			}
+		}
 	}
 }
 
 function Tile({game, group, grid, x, y, value, events}){
 	var self = this;
-	var g = game.add.graphics(
+	var g = game.add.sprite(
 		x*(grid.tileWidth + grid.margin),
 		y*(grid.tileHeight + grid.margin), 
-		group
+		"tile",
+		value
 	);
+	group.add(g);
 	g.inputEnabled = true;
 	g.events.onInputDown.add(() => events.click(self));
-	var redraw = this.redraw = function(){
-		g.clear();
-		g.lineStyle(2, 0xffffff, 1);
-		g.beginFill(0xf00ba5, 1)
-			.drawRect(0, 0, grid.tileWidth, grid.tileHeight)
-			.endFill();
-		if(value){
-			g.lineStyle(10, 0, 1);
-			g.beginFill(0, 1)
-				.moveTo(grid.tileWidth/2, grid.tileHeight/6)
-				.lineTo(grid.tileWidth/2, grid.tileHeight*5/6)
-				.endFill();
-		}
-	}
-	redraw();
 	
-	this.moveAndBack = function(dx, dy, cb){
+	this.moveAndBack = function(dx, dy, flag){
 		var gx = g.x,
 			gy = g.y;
-		if(cb){
+		events.beginTween();
+		if(flag){
 			game.add.tween(g).to({
 				x: (x + dx)*(grid.tileWidth + grid.margin), 
 				y: (y + dy)*(grid.tileHeight + grid.margin)
@@ -137,33 +221,43 @@ function Tile({game, group, grid, x, y, value, events}){
 			.onComplete.add(function(){
 				g.x = gx;
 				g.y = gy;
-				cb();
+				events.endTween();
 			});
 
 		}else{
 			g.x = (x + dx)*(grid.tileWidth + grid.margin);
 			g.y = (y + dy)*(grid.tileHeight + grid.margin);	
-			game.add.tween(g).to({x: gx, y: gy}, 300, "Linear", true);
+			game.add.tween(g).to({x: gx, y: gy}, 300, "Linear", true)
+				.onComplete.add(events.endTween);
 		}
+
 	}
 	
 	this.switch = function(){
 		value = 1 - value;
-		redraw();
+		g.frame = value;
 	}
 
+	this.destroy = function(){
+		g.destroy();
+	}
+
+	this.moveX = function(_){
+		x = _;
+		game.add.tween(g).to({x: x*(grid.tileWidth + grid.margin)}, 300, "Linear", true);
+	}
 	Object.defineProperties(this, {
 		value: {
 			get: () => value,
 			set: function(_){
 				value = _;
-				redraw();
+				g.frame = value;
 			}
 		},
 		x: {
 			get: () => x,
 			set: function(_){
-				x = _;
+				x = _;			
 				g.x = x*(grid.tileWidth + grid.margin);
 			}
 		},
